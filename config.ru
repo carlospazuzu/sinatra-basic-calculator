942f7f22b7d8fb020c1ad5265f86c9cc2e7b21d1
carlospazuzu <carlosh555@hotmail.com>
2023-01-26 23:06:26 -0300

    add: index view and rack configuration

---
+++ config.ru
@@ -0,0 +1,5 @@
+require_relative 'server'
+
+server = Server.new
+
+run server
--- server.rb
+++ server.rb
@@ -1,5 +1,17 @@
-require 'sinatra'
+require 'sinatra/base'
+require_relative 'entities/calculator'
 
-get '/' do
-  erb :index, locals: { name: 'Pazuzu' }
+class Server < Sinatra::Base
+  get '/' do
+    erb :index, locals: { result: 0 }
+  end
+
+  post '/' do
+    operand1 = params[:operand1].to_f
+    operand2 = params[:operand2].to_f
+    sign = params[:sign]
+    result = Calculator.calculate(operand1, operand2, sign)
+    erb :index, locals: { result: result }
+  end
 end
+
